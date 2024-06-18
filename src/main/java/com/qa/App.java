1a2c76926f4124354cd3c15e103df6e62b8abe7d
Steve Thomson <stephenthomson10@gmail.com>
2024-06-18 16:18:31 +0100

    added Tellis name to hello java app

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Steve"));
+        System.out.println(sayHelloToSomeone("Tellis"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
